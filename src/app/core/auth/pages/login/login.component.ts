a6ce3b563db9e5ad4841fd7f089575170ddd2dfb
MuhammedYousrii <muhammed.yuosry@gmail.com>
2024-03-31 07:23:21 +0200

    [Core][Auth]: Add the basic actions to login page

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,12 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import {  Router, RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  readonly FB = inject(FormBuilder);
+  readonly router = inject(Router);
+  readonly authService = inject(AuthService);
+
+  errorMessage: string | null = null;
+
+  readonly loginForm = this.FB.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  }) 
+
+
+  uponSubmit(): void {
+    const rawValue = this.loginForm.getRawValue();
+    this.authService.login(rawValue.email, rawValue.password).subscribe({
+      next: () => this.router.navigateByUrl('/'),
+      error: (error) => this.errorMessage = error.message
+    });
+  }
+
+  navigateToRegisterPage($e: Event): void {
+    $e.preventDefault();
+    this.router.navigateByUrl('/register');
+  }
+}
