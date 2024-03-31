45afbb4443039c87c2a8b0d87b6cc9e22658636e
MuhammedYousrii <muhammed.yuosry@gmail.com>
2024-03-31 17:31:42 +0200

    [Core](Auth): Use newly authService navigation methods.  Provide comments and omit unused code

--- src/app/core/auth/guards/auth.guard.ts
+++ src/app/core/auth/guards/auth.guard.ts
@@ -1,15 +1,18 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { firstValueFrom } from 'rxjs';
 
+/**
+ * @description Check If current user is authenticated or not and behave accordingly
+ * @returns {CanActivateFn}
+ */
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
-  const router = inject(Router);
 
   return firstValueFrom(authService.user$).then((user) => { 
     if (user) return true;
-    router.navigateByUrl('/auth/login');
+    authService.navigateToLoginPage();
     return false;
   })
 };
--- src/app/core/auth/guards/guest.guard.ts
+++ src/app/core/auth/guards/guest.guard.ts
@@ -1,15 +1,20 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { firstValueFrom } from 'rxjs';
 
+
+/**
+ * @description Check If current user is authenticated or not and behave accordingly
+ * @note It depends on the business requirement, where we should protect Guest routes or not
+ * @returns {CanActivateFn}
+ */
 export const guestGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
-  const router = inject(Router);
 
   return firstValueFrom(authService.user$).then((user) => { 
     if (!user) return true;
-    router.navigateByUrl('/converter');
+    authService.navigateToHomePage();
     return false;
   })
 };
