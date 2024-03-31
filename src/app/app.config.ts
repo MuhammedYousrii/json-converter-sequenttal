fb6a3b9e8c124901e09c8ad58ffce45737a648ca
MuhammedYousrii <muhammed.yuosry@gmail.com>
2024-03-31 07:19:31 +0200

    [Core]: Install @angular/fire to handle authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp,  } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+import { firebaseConfig } from './core/firebase.config';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(appRoutes, withViewTransitions()), 
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideAuth(() => getAuth()),
+
+    )
+
+  ],
+    
 };
---
+++ src/app/core/firebase.config.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA3SjC-myKmFVkZrnSc8yOymkBK0ukthsA",
+  authDomain: "json-converter-sequenttal.firebaseapp.com",
+  projectId: "json-converter-sequenttal",
+  storageBucket: "json-converter-sequenttal.appspot.com",
+  messagingSenderId: "966538022607",
+  appId: "1:966538022607:web:c8e0632cf09cae822331ba"
+};
