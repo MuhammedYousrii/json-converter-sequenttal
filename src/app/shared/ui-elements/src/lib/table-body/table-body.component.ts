b1d69ce4f593ce4a08b2cc192658f9dd40e39268
MuhammedYousrii <muhammed.yuosry@gmail.com>
2024-03-31 12:23:35 +0200

    [Shared](Table-body): Provide search box - Fix pagination and sort doesn't work from first hit

--- src/app/shared/ui-elements/src/lib/table-body/table-body.component.ts
+++ src/app/shared/ui-elements/src/lib/table-body/table-body.component.ts
@@ -1,30 +1,38 @@
-  import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+  import { AfterViewInit, ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
   import { MatTableDataSource, MatTableModule } from '@angular/material/table';
   import { MatSort, MatSortModule } from '@angular/material/sort';
-  import { Subject, takeUntil } from 'rxjs';
+  import { Subject, debounceTime, distinctUntilChanged, filter, takeUntil } from 'rxjs';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
   @Component({
     selector: 'ui-table-body',
     standalone: true,
-    imports: [CommonModule, MatTableModule, MatSortModule, MatPaginatorModule],
+    imports: [CommonModule, ReactiveFormsModule, MatTableModule, MatSortModule, MatPaginatorModule, MatFormFieldModule, MatInputModule],
     templateUrl: './table-body.component.html',
     styleUrls: ['./table-body.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
   })
-  export class TableBodyComponent<T> implements OnInit, OnDestroy {
+  export class TableBodyComponent<T> implements OnInit, OnDestroy, AfterViewInit {
     
     @Input() dataSource$: Subject<T[]> = new Subject<T[]>();
+    @Input() pageSizeOptions: number[] = [5, 10, 25, 100];
+    @Input() pageSize = 5;
+
     private _destroy$ = new Subject<void>();
 
     public tableDataSource = new MatTableDataSource<T, MatPaginator>([]);
     public displayedColumns: string[] = [];
-    readonly pageSizeOptions = [5, 10, 25, 100];
-    readonly pageSize = 5;
 
-    @ViewChild(MatSort, { static: false}) matSort!: MatSort;
-    @ViewChild(MatPaginator, {static: false}) matPaginator!: MatPaginator;
+
+    public searchControl = new FormControl('')
+
+
+    @ViewChild(MatSort) matSort!: MatSort;
+    @ViewChild(MatPaginator) matPaginator!: MatPaginator;
 
 
     ngOnInit(): void {
@@ -32,9 +40,23 @@ import { CommonModule } from '@angular/common';
         takeUntil(this._destroy$)).subscribe((data: T[]) => {
         this.tableDataSource.data = data;
         this.displayedColumns = data.length > 0 ? Object.keys((data[0] as any)) : [];
-        if (!this.tableDataSource.sort) this.tableDataSource.sort = this.matSort;
-        if (!this.tableDataSource.paginator) this.tableDataSource.paginator = this.matPaginator;
+       
       });
+
+      this.searchControl.valueChanges.pipe(
+        takeUntil(this._destroy$),
+        debounceTime(1000),
+        distinctUntilChanged(),
+        filter(searchValue => searchValue !== null),
+      ).subscribe(searchValue => {
+        this.tableDataSource.filter = searchValue || '';
+      })
+    }
+
+
+    ngAfterViewInit(): void {
+      if (!this.tableDataSource.sort) this.tableDataSource.sort = this.matSort;
+      if (!this.tableDataSource.paginator) this.tableDataSource.paginator = this.matPaginator;
     }
 
    
