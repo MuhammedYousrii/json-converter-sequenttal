fb72de971766d99f3808336dd1c4da5e530fa5c6
MuhammedYousrii <muhammed.yuosry@gmail.com>
2024-03-31 20:05:27 +0200

    [Shared](Ui-elements): Create capitalize pipe for using inside table and filters
    also I have exported it to any interested consumer

--- src/app/shared/ui-elements/src/index.ts
+++ src/app/shared/ui-elements/src/index.ts
@@ -1,6 +1,11 @@
+// Components export
 export * from './lib/page-not-found/page-not-found.component';
 export * from './lib/empty-result/empty-result.component';
 export * from './lib/table-body/table-body.component';
 export * from './lib/filter-box/filter-box.component';
 export * from './lib/pagination/pagination.component';
 export * from './lib/spinner/spinner.component';
+
+
+// Pipes export
+export * from './lib/pipes';
